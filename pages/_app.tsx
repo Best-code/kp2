fb5e45217150b1f19cdb285d767a8e9c0cd289b7
Best-Code <cpmraygun@gmail.com>
2022-01-23 14:07:25 -0500

    META Tag for google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import { SessionProvider } from "next-auth/react"
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return <SessionProvider session={session}>
     <Head>
-      <meta name="google-site-verificaiton" content="ZOz3YH6HxJN9BfJOhvbSwpLSrGDXo8HNJE18utyRFJI"/>
+      <meta name="google-site-verification" content="DuBVzA_HUhu_tJFZ8IW1HVdhApi3iwjZK035sGMxMrE" />
       <title>Mrs. Kilpatricks Awesome Website</title>
       <meta name="description" content="Created by Colin Maloney" />
       <link rel="icon" href="/favicon.ico" />
