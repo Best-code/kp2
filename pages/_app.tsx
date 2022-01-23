d927674ec8b6cb5fb4c9d8c5a47d038f39475570
Best-Code <cpmraygun@gmail.com>
2022-01-23 13:57:15 -0500

    For google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import { SessionProvider } from "next-auth/react"
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return <SessionProvider session={session}>
     <Head>
+      <meta name="google-site-verificaiton" content="ZOz3YH6HxJN9BfJOhvbSwpLSrGDXo8HNJE18utyRFJI"/>
       <title>Mrs. Kilpatricks Awesome Website</title>
       <meta name="description" content="Created by Colin Maloney" />
       <link rel="icon" href="/favicon.ico" />
