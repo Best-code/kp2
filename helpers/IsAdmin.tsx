e2fb8738ef4e765922a7c7c2c06d5841ec43ac0e
Best-code <cpmraygun@gmail.com>
2022-01-24 13:14:30 -0500

    LocalHost Admin Fix

--- helpers/IsAdmin.tsx
+++ helpers/IsAdmin.tsx
@@ -1,9 +1,10 @@
 import { Session } from 'next-auth';
+import {serverRoute} from "../config"
 
 export const IsAdmin = async (session: Session | null) => {
     let isAdmin = false
     if (session && session.user) {
-        const adminRes = await fetch(`http://localhost:3000/api/admin?email=${session.user.email}`)
+        const adminRes = await fetch(`${serverRoute}/api/admin?email=${session.user.email}`)
         isAdmin = await adminRes.json()
     } else {
         isAdmin = false
