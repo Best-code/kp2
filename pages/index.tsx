f891e979d76aceffa8a42be65b1393b9db3a76d3
Best-code <cpmraygun@gmail.com>
2022-01-24 12:48:51 -0500

    Idek

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { GetServerSideProps, NextPage } from 'next'
+import { serverRoute } from "../config"
 
-const Home: NextPage = ({widgetLoaded} : any) => {
+const Home: NextPage = ({ widgetLoaded }: any) => {
 
-   return <div className="lg:flex">
+  return <div className="lg:flex">
     <div className="flex flex-col px-8 items-center">
       <h1 className="text-center text-4xl  lg:text-6xl font-bold pt-10 md:pb-12 lg:pb-16 xl:pb-0 w-screen md:w-full">
         Welcome to Mrs. Kilpatricks Science Webite
+        {serverRoute}
       </h1>
       <div className="flex lg:w-4/5 justify-center items-center md:h-2/3 h-screen">
         <p className="text-left text-2xl md:text-3xl  font-semibold pb-32 md:pb-0">
@@ -33,10 +35,10 @@ const Home: NextPage = ({widgetLoaded} : any) => {
       </div>
     </div>
     {widgetLoaded && (
-    <div className="flex w-screen justify-center overflow-scroll h-screen">
-      <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-      <a className="flex twitter-timeline" href="https://twitter.com/Chemteach84" data-width="399" ></a>
-    </div>
+      <div className="flex w-screen justify-center overflow-scroll h-screen">
+        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+        <a className="flex twitter-timeline" href="https://twitter.com/Chemteach84" data-width="399" ></a>
+      </div>
     )}
   </div>
 }
@@ -44,8 +46,15 @@ const Home: NextPage = ({widgetLoaded} : any) => {
 export default Home
 
 
-export const getServerSideProps : GetServerSideProps = async (context) => {
-  const widgetRes = await fetch("https://platform.twitter.com/widgets.js")
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+  let widgetRes : any;
+  try {
+    widgetRes = await fetch("https://platform.twitter.com/widgets.js")
+  } catch (error) {
+    widgetRes = false;
+  }
+
   const widgetLoaded = widgetRes ? true : false;
-  return {props:{widgetLoaded}}
+  return { props: { widgetLoaded } }
 }
